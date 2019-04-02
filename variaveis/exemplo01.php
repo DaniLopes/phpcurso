8f141140f0444b9d07cb2977c31b8a7b1337550c
Daniele de Souza <daniele.lopes.f@gmail.com>
2019-04-02 17:36:21 -0300

    Tipos de dados

--- variaveis/exemplo01.php
+++ variaveis/exemplo01.php
@@ -1,18 +1,42 @@
 <?php
 //tipos basicos
 $nome = "Dani";
-$anoNasc = 1992;
+$anoNasc = new DateTime();
 $salario = 5500.99;
 $bloq = false;
 
+//variáveis pré definidas
+
+//$nome = (int)$_GET["a"];
+
+//var_dump($nome);
+//$ip = $_SERVER["REMOTE_ADDR"]; //IP DO PROVEDOR DE ACESSO.
+$ip = $_SERVER["SCRIPT_NAME"]; //LOG DE ACESSO.
+echo $ip;
+
+
+
+
+exit();
 //tipos compostos
 
 $frutas = array("abacaxi","laranja","manga","apple");
 
 
+//var_dump($anoNasc);
+
+
+//tipos especiais
+$arquivo = fopen("exemplo01.php","r");
+//var_dump($arquivo);
+
+$nulo = NULL; //AUSENCIA DE VALOR //VAZIO FOI INICIADO mas já foi reservado um espaço na memoria.
+
+var_dump($nulo);
+
 exit;
 $nome = "Dani";
-$anoNasc = "1992";
+
 
 echo $nome." nasceu no ano de". $anoNasc;
 //unset($anoNasc);
